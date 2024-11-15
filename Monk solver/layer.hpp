2752aaf264c1f5e3986f15818a596a9c29a805e4
FraLiturri <francesco.liturri.dev@gmail.com>
2024-11-15 18:04:02 +0100

    Updated Monk solver

--- Monk solver/layer.hpp
+++ Monk solver/layer.hpp
@@ -11,34 +11,41 @@ using namespace std;
 using namespace Eigen;
 
 // Defining networks variables;
-int hidden_layers = 1;
-int in_units = 6;
-int out_units = 6;
+const int in_units = 1;  // Number of units in input layer;
+const int out_units = 2; // Number of units in the output layer;
 
-Vector2d hidden_units = {6, 3}; // Each component represents the numbers of unit in each HIDDEN layer;
+int hidden_layers = 4;                // Number of hidden layers;
+VectorXd hidden_units(hidden_layers); // Each component represents the numbers of unit in each HIDDEN layer;
 
-// Tot number of units;
-int units = 1;
-int number()
+// How many units?
+double tot_units;
+double counter()
 {
+    double count = 1;
     for (int i = 0; i < hidden_units.size(); i++)
     {
-        units += hidden_units[i];
+        count *= hidden_units[i];
     }
-    units = units * in_units * out_units;
-    return units;
+    tot_units = count * in_units * out_units;
+    return tot_units;
 }
 
-VectorXd weights(units);
-
-double Layer(string function_choosen, Vector3d input) //In future has to return a vector;
+//General hidden layer; 
+double Layer(string function_choosen, Vector3d input) // In future has to return a vector;
 {
     func_choiser(function_choosen); //! use act_func as working tool [e.g. act_func(2.3)];
     return 0;
 }
 
-double inLayer(VectorXd input) //Input layer; 
+//Input layer; 
+double input_Layer(VectorXd input) // Input layer;
 {
+    int next_units = int(hidden_units[0]); 
+    VectorXd output_0(next_units);
+    for(int i = 0; i < int(hidden_units[0]); i++){
+         output_0[i] = 1; 
+    }
+    cout << output_0 ; 
     return 0;
 }
 
--- Monk solver/main.cpp
+++ Monk solver/main.cpp
@@ -1,14 +1,21 @@
 #include "layer.hpp"
 #include "activation_functions.hpp"
-
 #include <iostream>
 #include <string>
 #include <random>
+#include "C:/Users/franc/OneDrive/Desktop/Sync/Eigen/Eigen/Dense" //works
 
+using namespace Eigen;
 using namespace std;
 
 int main()
-{  
-    Layer("linea", {0,0,3}); //test printing act_func(double); 
-    return 0; 
+{
+    //! Initialization part: do not delete;
+    hidden_units << 1, 2, 3, 4; // Number of units in each hidden layer;
+    counter();                  // Counts how many units have been used;
+
+    // Neural network construction;
+    Layer("linear", {0, 0, 3}); // test printing act_func(double);
+    input_Layer({0, 1});
+    return 0;
 }
