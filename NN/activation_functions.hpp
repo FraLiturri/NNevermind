17130f699907ece7847f82c1b57821ce8ee5aa05
Calisse <lapodispoto@gmail.com>
2024-12-21 22:54:59 +0100

    Modified trace of Hidden_Layer::BackPropagation

---
+++ NN/activation_functions.hpp
@@ -0,0 +1,86 @@
+#ifndef activation_functions
+#define activation_functions
+
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+// Defining activation functions and their derivatives (needed for Backpropagation);
+double sigmoid(double x)
+{
+    return 1 / (double)(1 + exp(-x)); // Is "a" needed? (e^-ax);
+}
+double sigmoid_der(double x)
+{
+    return sigmoid(x) * (1 - sigmoid(x));
+}
+
+double linear(double x)
+{
+    return x;
+}
+double linear_der(double x)
+{
+    return 1;
+}
+
+double threshold(double x)
+{
+    double threshold;
+    x > 0 ? threshold = 1 : threshold = 0;
+    return threshold;
+}
+double threshold_der(double x)
+{
+    return 0;
+}
+
+double relu(double x)
+{
+    double relu_res;
+    x < 0 ? relu_res = 0 : relu_res = x;
+    return relu_res;
+}
+double relu_der(double x)
+{
+    double relu_res;
+    x < 0 ? relu_res = 0 : relu_res = 1;
+    return relu_res;
+}
+
+// Defining pointer to activation function(s) and derivative(s);
+double (*act_func)(double);     // pointer to f;
+double (*der_act_func)(double); // pointer to f';
+
+// Choosing activation function and its derivative;
+void func_choiser(std::string choice)
+{
+    if (choice == "sigmoid")
+    {
+        act_func = sigmoid;
+        der_act_func = sigmoid_der;
+    }
+    else if (choice == "linear")
+    {
+        act_func = linear;
+        der_act_func = linear_der;
+    }
+    else if (choice == "threshold")
+    {
+        act_func = threshold;
+        der_act_func = threshold_der;
+    }
+    else if (choice == "relu")
+    {
+        act_func = relu;
+        der_act_func = relu_der;
+    }
+    else
+    {
+        cout << "\nChoice unavailable.\n"
+             << endl;
+    }
+}
+
+#endif
---
+++ NN/training.hpp
@@ -0,0 +1,136 @@
+#ifndef training
+#define training
+
+#include "layer.hpp"
+#include "demiurge.hpp"
+#include "activation_functions.hpp"
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include "C:/Users/franc/OneDrive/Desktop/Sync/Eigen/Eigen/Dense"
+
+using namespace std;
+using namespace Eigen;
+
+VectorXd delta, net_t, net;
+vector<VectorXd> storer;
+MatrixXd update, auxiliar, prev_weight;
+
+double delta_k = 0; // auxiliar double;
+int i;              // Runs over wieghts matrices;
+
+VectorXd net_calculator(int layer_number)
+{
+    net = weights[layer_number - 1] * outputs[layer_number - 1];
+    return net;
+};
+
+void Hidden_Layer::BackPropagation(VectorXd d, double eta, double alpha, double lambda)
+{
+    //aggiustare d, ho modificato la classe in input da double a VectorXd
+
+    i = weights.size();
+    while (i > 0)
+    {
+        prev_weight = weights[i - 1]; // For Tikhonov regularization;
+
+        if (i == weights.size())
+        {
+            delta.setZero();
+            func_choiser(function_strings[i - 1]);
+            net_t = net_calculator(i);
+
+            for (int k = 0; k < 1; k++)
+            {
+                delta.conservativeResize(k + 1);
+                delta_k = (d - outputs[i][k]) * der_act_func(net_t[k]);
+                delta[k] = delta_k;
+            }
+
+            if (prev_updates[0](0, 0) == 0)
+            {
+                update = eta * delta * outputs[i - 1].transpose() - lambda * prev_weight;
+            }
+            else
+            {
+                update = eta * delta * outputs[i - 1].transpose() - lambda * prev_weight + alpha * prev_updates[i - 1];
+            }
+
+            weights[i - 1] = weights[i - 1] + update;
+            weights[i - 1].col(0).setConstant(1);
+            prev_updates[i - 1] = update;
+            storer.push_back(delta);
+        }
+
+        else
+        {
+            delta.setZero();
+            func_choiser(function_strings[i - 1]);
+
+            auxiliar = weights[i];
+            net_t = net_calculator(i);
+
+            delta = storer[weights.size() - (i + 1)].transpose() * auxiliar;
+
+            for (int k = 0; k < delta.size(); k++)
+            {
+                delta[k] = delta[k] * der_act_func(net_t[k]);
+            }
+
+            if (prev_updates[0](0, 0) == 0)
+            {
+                update = eta * delta * outputs[i - 1].transpose() - lambda * prev_weight;
+            }
+            else
+            {
+                update = eta * delta * outputs[i - 1].transpose() - lambda * prev_weight + alpha * prev_updates[i - 1];
+            }
+
+            weights[i - 1] = weights[i - 1] + update;
+            weights[i - 1].col(0).setConstant(1);
+            prev_updates[i - 1] = update;
+
+            storer.push_back(delta);
+        }
+
+        if (i == 1)
+        {
+            outputs.clear();
+            next_inputs.clear();
+            function_strings.clear();
+            storer.clear();
+        }
+        i--;
+    };
+}
+
+void Hidden_Layer::RandomTraining(VectorXd d, double eta, double alpha = 0, double lambda = 0)
+{
+    //aggiustare d, ho modificato la classe in input da double a VectorXd
+
+    i = weights.size();
+    delta.setZero();
+
+    func_choiser(function_strings[i - 1]);
+    net_t = net_calculator(i);
+
+    for (int k = 0; k < 1; k++)
+    {
+        delta.conservativeResize(k + 1);
+        delta_k = (d - outputs[i][k]) * der_act_func(net_t[k]);
+        delta[k] = delta_k;
+    }
+
+    update = delta * outputs[i - 1].transpose();
+    weights[i - 1] = weights[i - 1] + eta * update;
+    weights[i - 1].col(0).setConstant(1);
+
+    function_strings.clear();
+    outputs.clear();
+    next_inputs.clear();
+    units_output.setZero();
+}
+
+#endif
