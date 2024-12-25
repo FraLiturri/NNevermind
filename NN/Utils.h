f75b7c664f5ee94dda17849a3e2a1b37671fb05c
Calisse <lapodispoto@gmail.com>
2024-12-25 12:08:58 +0100

    Added alpha trial of grid search, to be integrated with a true main, to be adapted to eigen structure

---
+++ NN/Utils.h
@@ -0,0 +1,64 @@
+#include<vector>
+#include<tuple>
+#include<iostream>
+#include <random>
+
+
+struct HyperParameters
+{
+    double eta;
+    double lambda;
+    double alpha;
+}
+
+
+
+std::vector<HyperParameters> generateGrid(double min1, double max1, double min2, double max2, double min3, double max3, int steps)
+{
+    std::vector<HyperParameters>grid;
+    double StepSize1 = (max1-min1)/(steps-1)
+    double StepSize2 = (max2-min2)/(steps-1)
+    double StepSize3 = (max3-min3)/(steps-1)
+
+    for(int i = 0 ; i<steps; i++)
+    {
+        for(int j =0; j<steps; j++)
+        {
+            for(int k =0;k<steps;k++)
+            {
+                grid.push_back(
+                    min1 + i*StepSize1,
+                    min2 +j*StepSize2,
+                    min3 +k*StepSize3
+                )
+            }    
+        }
+    }
+
+    return grid
+};
+
+
+std::vector<int> generateRandomIndices(int DataLength, double percentual)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd()); //scelgo un generatore a caso di numeri casuali
+    std::vector<int> indices(grid.size());
+    //creo il vettore di indici originale
+    for(int j =0; j<DataLength; j++)
+    {
+        indices[j] = j;
+    }
+    //faccio uno shuffle
+    std::shuffle(indices.begin(), indices.end(), gen);
+
+    //ora per selezionare la percentuale di grid search, semplicemente prendo i primi N su grid.size numeri, dove N/M e' la percentuale.
+    int N = std::round(DataLength*percentual);
+    std::vector<int> RandomIndices(N)
+    for(int j = 0; j<N; j++)
+    {
+        RandomIndices[j] = indices[j];
+    }
+    return RandomIndices
+
+}
--- NN/main.cpp
+++ NN/main.cpp
@@ -3,30 +3,51 @@
 #include <random>
 #include <chrono>
 #include <cmath>
-
+#include<omp.h>
+#include"Utils.h"
 #include "C:/Users/franc/OneDrive/Desktop/Sync/Eigen/Eigen/Dense"
 
 using namespace std;
 
-double f(int &x)
+void TrueMain(HyperParameters params)
 {
-    return x * x;
+    std::cout
+    //put here training and stuff 
+
 }
 
-int main()
-{
-    auto start = chrono::high_resolution_clock::now();
 
-    for (int k = 0; k < 2000000; k++)
+
+int main(int argc char *argv[])
+{
+    omp_set_num_threads(32); //find a way to tune this parameter automatically...
+
+    //genero lo spazio delle fasi ed accoppio i valori degli iperparametri agli indici.
+    auto grid = generateGrid(argv[1], argv[2],argv[3],argv[4],argv[5],argv[6],argv[7]);
+    std::vector<std::pair<HyperParameters, double>> HParamsAndResults(grid.size());
+    auto start = chrono::high_resolution_clock::now();
+    //parte con indici non randomici, itera su tutto lo spazio delle fasi
+
+    #pragma omp parallel for //direttiva omp per parallelizzazione
+    for (int k = 0; k < grid.size(); k++)
     {
-        f(k);
+        TrueMain(grid[k]);
     }
 
     // Counter stops and prints elapsed time;
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed_time = end - start;
-    cout << "Elapsed time: " << elapsed_time.count() << " seconds.\n"
+    cout << "Elapsed time for sequential: " << elapsed_time.count() << " seconds.\n"
          << endl;
+
+    //parte fatta con indici randomici, e' uguale a quella precedente se non per l'indicizzazione
+    std::vector<int> RandomIndices = generateRandomIndices(grid.size(), 0.8); 
+    #pragma omp parallel for //direttiva omp per parallelizzazione
+    for(int k =0, k<RandomIndices.size(); k++)
+    {
+        TrueMain(grid(RandomIndices(k)));
+    }
+
 
     return 0;
 }
